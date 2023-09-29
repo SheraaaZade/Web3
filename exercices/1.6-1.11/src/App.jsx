6a77abef7980bf11084a52780b268d4d5b2e38b5
Chehrazad <chehrazad.ouazzani@student.vinci.be>
2023-09-29 12:22:11 +0200

    1.7 done

--- exercices/1.6-1.11/src/App.jsx
+++ exercices/1.6-1.11/src/App.jsx
@@ -19,6 +19,10 @@ const App = () => {
         setBad(bad + 1);
     }
 
+    const totalFeedback = good + neutral + bad;
+    const averageScore = (good - bad) / totalFeedback;
+    const positiveFeedbackPercentage = (good / totalFeedback) * 100;
+
 
     return (
         <div>
@@ -27,10 +31,9 @@ const App = () => {
             <button onClick={handleNeutralClick}> Neutral</button>
             <button onClick={handleBadClick}> Bad</button>
 
-            <h2>Statistics</h2>
-            <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
+            <p>Total feedback: {totalFeedback}</p>
+            <p>Average score: {isNaN(averageScore) ? 0 : averageScore.toFixed(2)}</p>
+            <p>Positive feedback percentage: {isNaN(positiveFeedbackPercentage) ? 0 : positiveFeedbackPercentage.toFixed(2)}%</p>
         </div>
     )
 }
