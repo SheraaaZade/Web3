40ecf915fbdd32410e228b25a2bf04ef630f4913
Chehrazad <chehrazad.ouazzani@student.vinci.be>
2023-10-27 14:32:48 +0200

    corrected module6

--- exercices/module6/index.js
+++ exercices/module6/index.js
@@ -1,70 +1,60 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
 
-const Note = require('./note')
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
 
-let notes = [
-]
+const password = process.argv[2]
 
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const url =
+`mongodb+srv://chehrazadouazzani:${password}@helloworld.atoynja.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+    response.json(notes);
+  }).catch(error => {
+    console.error('Error fetching notes:', error);
+    response.status(500).json({ error: 'Internal Server Error' });
+  });
+});
 
 app.post('/api/notes', (request, response) => {
-  const body = request.body
+  const body = request.body;
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+  if (!body.content || body.content.trim() === '') {
+    return response.status(400).json({ error: 'Content is required' });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  })
+  });
 
   note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
+    response.json(savedNote);
+  }).catch(error => {
+    console.error('Error saving note:', error);
+    response.status(500).json({ error: 'Internal Server Error' });
+  });
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
