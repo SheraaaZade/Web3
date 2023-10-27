2d4b0282e63228e4ee342a3c62848e1d66f4f32b
Chehrazad <chehrazad.ouazzani@student.vinci.be>
2023-10-27 13:43:20 +0200

    module6

--- exercices/module6/index.js
+++ exercices/module6/index.js
@@ -1,126 +1,70 @@
-const express = require("express")
-const morgan = require('morgan')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
 
+const Note = require('./note')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-// Data. This emulates a datastore
-const allPersons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada lovelace",
-    number: "39-44-5232323",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-2345345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-64234122",
-  },
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
+
+let notes = [
 ]
 
 
-// Initializers. These blocks should be placed in different files, for example middlewares.js, server.js...
-// but let's keep this simple.
-const app = express()
-
-app.use(express.json())
-
-morgan.token('body', (request) => {
-  return JSON.stringify(request.body)
-})
-morgan.token('currentUserEmail', (request) => {
-  return request.currentUser && request.currentUser.email || "anonymous"
-})
-const logger = morgan(':method :url :status :res[content-length] - :response-time ms :body (:currentUserEmail)')
-app.use(logger)
-
-const attachCurrentuser = (request, response, next) => {
-  const email = request.header("X-USER-EMAIL")
-  if (email) request.currentUser = { email }
-  next()
-}
-app.use(attachCurrentuser)
-
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-
-// Routes. These blocks should be placed in different files under "routes/" directory
-// but let's keep this simple.
-app.get("/info", (request, response) => {
-  const now = new Date()
-  const bodyContentText = `
-  Phonebook has info for ${allPersons.length} people.
-  ${now.toString()}
-  `
-  response
-    .type("text")
-    .send(bodyContentText)
-})
-
-app.get("/api/persons", (request, response) => {
-  response.json(allPersons)
-})
-
-app.get("/api/persons/:id", (request, response) => {
-  const idParam = request.params.id
-  const id = Number(idParam)
-  const person = allPersons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  const idParam = request.params.id
-  const id = Number(idParam)
-  const personIndex = allPersons.findIndex(person => person.id === id)
-  if (personIndex > -1) {
-    allPersons.splice(personIndex, 1) // We do NOT use delete because it creates a sparse array with a wrong length
-  }
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
   response.status(204).end()
 })
 
-app.post("/api/persons", (request, response) => {
-  const personPayload = request.body
-  const newId = Math.floor(Math.random() * 1e9)
-  const newPerson = {
-    ...personPayload,
-    id: newId,
-  }
+app.use(unknownEndpoint)
 
-  const errorMessages = []
-  if (!personPayload.name) errorMessages.push("name must be present")
-  if (!personPayload.number) errorMessages.push("number must be present")
-  const nameExists = allPersons.some(person => person.name === newPerson.name)
-  if (nameExists) errorMessages.push("name must be unique")
-
-  if (errorMessages.length > 0) {
-    response
-      .status(422)
-      .json({
-        errorMessages,
-      })
-    return
-  }
-
-  // push not concat here. We want to mutate the array.
-  allPersons.push(newPerson)
-  response.json(newPerson)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
--- exercices/module6/mongo.js
+++ exercices/module6/mongo.js
@@ -10,17 +10,31 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://chehrazadouazzani:${password}@helloworld.atoynja.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-Note.find({}).then(result => {
+  mongoose.set('strictQuery',false)
+  mongoose.connect(url)
+  
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+  })
+  
+  const Note = mongoose.model('Note', noteSchema)
+  
+  const note = new Note({
+    content: 'Mongoose makes things easy',
+    date: new Date(),
+    important: true,
+  })
+  
+  /*
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  
+  Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
     })
